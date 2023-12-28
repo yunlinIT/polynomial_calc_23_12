800f8b7324cb8e9478c432ec583f2ad177fb349a
yunlinIT <yunlinit@gmail.com>
2023-12-29 03:27:10 +0900

    10 * 10 * 10

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,19 +2,32 @@ package com.ll;
 
 public class Calc {
   public static int run(String exp) {
+    boolean needToMultiply = exp.contains("*");
+    boolean needToPlus = !needToMultiply;
 
-    exp = exp.replaceAll("\\- ", "\\+ \\-");
+    if (needToPlus) {
+      exp = exp.replaceAll("\\- ", "\\+ \\-");
 
-    String[] bits = exp.split(" \\+ ");
+      String[] bits = exp.split(" \\+ ");
 
-    int sum = 0;
+      int sum = 0;
 
-    for (int i = 0; i < bits.length; i++) {
-      sum += Integer.parseInt(bits[i]);
+      for (int i = 0; i < bits.length; i++) {
+        sum += Integer.parseInt(bits[i]);
+      }
+
+      return sum;
+    } else if (needToMultiply) {
+      String[] bits = exp.split(" \\* ");
+
+      int rs = 1;
+
+      for (int i = 0; i < bits.length; i++) {
+        rs *= Integer.parseInt(bits[i]);
+      }
+      return rs;
     }
 
-    return sum;
-
-//    throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
+    throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
   }
 }
--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -71,4 +71,22 @@ public class CalcTest {
   void t11() {
     assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
   }
+
+  @Test
+  @DisplayName("10 * 10 == 100")
+  void t12() {
+    assertThat(Calc.run("10 * 10")).isEqualTo(100);
+  }
+
+  @Test
+  @DisplayName("10 * -10 == -100")
+  void t13() {
+    assertThat(Calc.run("10 * -10")).isEqualTo(-100);
+  }
+
+  @Test
+  @DisplayName("10 * 10 * 10 == 1000")
+  void t14() {
+    assertThat(Calc.run("10 * 10 * 10")).isEqualTo(1000);
+  }
 }
