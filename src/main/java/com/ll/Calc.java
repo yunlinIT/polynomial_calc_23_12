4300fe18c50952ee027925e77022d3e9ece28e2c
yunlinIT <yunlinit@gmail.com>
2023-12-29 03:21:02 +0900

    10 + 20 + 30

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -16,11 +16,16 @@ public class Calc {
 
     int a = Integer.parseInt(bits[0]);
     int b = Integer.parseInt(bits[1]);
+    int c = 0;
+
+    if (bits.length > 2) {
+      c = Integer.parseInt(bits[2]);
+    }
 
     if (needToPlus) {
-      return a + b;
+      return a + b + c;
     } else if (needToMinus) {
-      return a - b;
+      return a - b - c;
     }
 
     throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -48,4 +48,9 @@ public class CalcTest {
     assertThat(Calc.run("3 - 1")).isEqualTo(2);
   }
 
+  @Test
+  @DisplayName("10 + 20 + 30 == 60")
+  void t8() {
+    assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
+  }
 }
